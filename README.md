9a34823f14a584d12d0860c543d0b787b1c45c4c
Eric Boersma <eric.boersma@gmail.com>
2015-02-17 16:52:28 -0500

    Adding file structure which allows for PyPi distribution

---
+++ README.md
@@ -0,0 +1,4 @@
+# shareprogress
+
+The shareprogress library is designed to be a Python interface to the [ShareProgress](http://www.shareprogress.org/)
+API. It provides a simple Plain Old Python Object interface to the API.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='shareprogress',
+    packages=['shareprogress'],
+    version='0.1',
+    description='A python library for interacting with the ShareProgress API.',
+    author='SumOfUs',
+    author_email='info@sumofus.org',
+    url='https://github.com/SumOfUs/shareprogress',
+    download_url='https://github.com/SumOfUs/shareprogress/tarball/0.1',
+    keywords=['api', 'shareprogress', 'sharing'],
+    classifiers=[],
+)
